0c3048a799ffeecdaaffa2de1cf8b583ad2d0bbd
Joby Eynon <joby@blockdog.io>
2021-09-06 20:58:18 +0000

    changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
      
+     //Interest rate slider 
      
      function updateRate() 
 {
@@ -7,22 +8,38 @@
     document.getElementById("rate_val").innerText=rateval;
 }
 
+  //Check for positive values & compute interest on principle 
+  
+  function compute() {
+    var principal = document.getElementById("principal").value;
+    var moreThenZero = parseInt(principal) > 0;
+    if (!moreThenZero) {
+        alert("Enter a positive number please");
+        document.getElementById("principal").focus();
+    }
 
-function compute()
+        else
+
      {
+
         var principal = document.getElementById("principal").value;
 
         var rate = document.getElementById("rate").value;
-   
+        
         var years = document.getElementById("years").value;
-   
+        
         var interest = principal * years * rate /100;
-   
-        var amount = parseInt(principal) + interest;
-   
+        
+        var amount =  interest;
+
+        var total = parseInt(principal) + interest;
+        
         var year = new Date().getFullYear()+parseInt(years);
 
-      p = document.getElementById("principal").nodeValue;
-         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    p = document.getElementById("principal").nodeValue;
+         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>Making your total "+total+"\<br\>"
          
      }
+  }
+    
+
